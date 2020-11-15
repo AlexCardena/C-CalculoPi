ec0a74dd66f9628d72380a212378a4967c809165
Alex Ruben <arcb2164@gmail.com>
2020-11-15 18:22:22 -0400

    Add files via upload

---
+++ EncontrarPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EncontrarPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			long num_pasos = 10000000;
+			double paso;
+			double sum,pi;
+			paso = 1.0 / (double)num_pasos;
+			double x;
+			sum = 0.0;	
+            #pragma omp parallel for private(i) reduction(+:sum) reduction(+:x)
+			for (int i = 0; i < num_pasos; i++)
+			{
+				x = (i + 0.5) * paso;
+				sum = sum + 4.0 / (1.0 + x * x);
+			}
+			pi = paso * sum;
+			Console.WriteLine("El valor de pi es "+ pi);
+		}
+    }
+}
